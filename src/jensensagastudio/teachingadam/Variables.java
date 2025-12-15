31164db17f7dab52397257fbe984da6cd790682a
Christian Daniel Jensen <cdjensen94@gmail.com>
2025-12-15 12:02:22 -0700

    Initial Commit

---
+++ src/jensensagastudio/teachingadam/Variables.java
@@ -0,0 +1,10 @@
+package jensensagastudio.teachingadam;
+
+public class Variables {
+    int myNumber = 5;
+    String myString = "Hello, World!";
+    char myChar = 'A';
+    boolean myBoolean = true;
+    double myDouble = 5.99;
+    float myFloat = 3.14f;
+}
